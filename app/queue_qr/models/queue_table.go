e3f031b88f5a1297272d4eec2d1a6508f7a13e00
Renal Apriansyah <renal.apr@gmail.com>
2023-01-04 15:08:23 +0700

    validate if the table is on the queue

--- app/queue_qr/models/queue_table.go
+++ app/queue_qr/models/queue_table.go
@@ -2,17 +2,9 @@ package models
 
 import (
 	"time"
-
-	enum "ant3/app/queue_qr/enum"
-
-	"github.com/google/uuid"
 )
 
 type QueueTable struct ***REMOVED***
-	Id uuid.UUID `json:"id"`
-	TableName string `json:"tableName"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	Status enum.QUEUE_STATUS `json:"status"`
-	QueueQR QueueQrDTO `json:"queueQr"`
 ***REMOVED***
--- app/queue_qr/service/add_table_to_queue.go
+++ app/queue_qr/service/add_table_to_queue.go
@@ -2,14 +2,12 @@ package service
 
 import (
 	fb "ant3/app/config/firebase"
-	"ant3/app/queue_qr/enum"
 	"ant3/app/queue_qr/models"
 	"ant3/app/queue_qr/repository"
 	"context"
+	"errors"
 	"fmt"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 func AddQRTableToQueue(queueQrId string) (*string, error) ***REMOVED***
@@ -22,22 +20,35 @@ func AddQRTableToQueue(queueQrId string) (*string, error) ***REMOVED***
 	now := time.Now().UTC()
 	
 	var queueTable = models.QueueTable***REMOVED***
-		Id: uuid.New(),
 		CreatedAt: now,
 		UpdatedAt: now,
-		Status: enum.PENDING,
-		QueueQR: *queueQr,
 	***REMOVED***	
 	
-	fmt.Printf("response queueTable: %#v", queueTable)
+	fmt.Printf("response queueTable: %#v\n", queueTable)
+	// Noted: try to validate using firebase rules
+	var existQueueQr models.QueueTable
 	
 	ref := fb.Client.NewRef("queue_table")
-	// ref.Child("queue_table").Path("QueueQr")
+	myErr := ref.
+		Child(queueQr.Id).
+		Get(context.Background(), &existQueueQr)
+		
+	if(myErr != nil) ***REMOVED***
+		fmt.Println("error get queueId: " + queueQr.Id, myErr.Error())
+	***REMOVED***
 	
-	result, err := ref.Push(context.Background(), queueTable)
-	if(err != nil) ***REMOVED***
+	fmt.Printf("response existQueueQr: %#v\n", existQueueQr)
+	
+	if (existQueueQr != models.QueueTable***REMOVED******REMOVED***) ***REMOVED***
+		return nil, errors.New("The " + queueQr.Id + " table is on the queue")
+	***REMOVED***
+	
+	
+	fbErr := ref.Child(queueQr.Id).
+		Set(context.Background(), &queueTable)
+	if(fbErr != nil) ***REMOVED***
 		fmt.Println(err, "[AddQRTableToQueue] error when insert to firebase")
 		return nil, err
 	***REMOVED***
-	return &result.Key, nil
+	return &queueQr.Id, nil
 ***REMOVED***
