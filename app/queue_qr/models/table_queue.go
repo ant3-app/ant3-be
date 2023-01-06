c92be19985a05d14aaecb815446c3587b9ee4877
Renal Apriansyah <renal.apr@gmail.com>
2023-01-06 18:59:07 +0700

    increment based on last num

--- app/queue_qr/models/table_queue.go
+++ app/queue_qr/models/table_queue.go
@@ -8,4 +8,6 @@ type TableQueue struct ***REMOVED***
 	Id string `json:"id"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
+	Name string `json:"name"`
+	Number int `json:"number"`
 ***REMOVED***
--- app/queue_qr/service/table_queue.go
+++ app/queue_qr/service/table_queue.go
@@ -7,7 +7,41 @@ import (
 	"errors"
 	"fmt"
 	"time"
+
+	"cloud.google.com/go/firestore"
 )
+
+func getLastDocNum(trx *firestore.Transaction, colRef *firestore.CollectionRef) (*int64, error) ***REMOVED***
+	query := colRef.OrderBy("createdAt", firestore.Desc).Limit(1)
+	lastDoc, err := trx.Documents(query).Next()
+	if(err != nil) ***REMOVED***
+		return nil, err
+	***REMOVED***
+	
+	numberLastDoc := lastDoc.Data()["number"].(int64)
+	
+	return &numberLastDoc, nil
+***REMOVED***
+
+func validateGenerateNewQueue(
+	trx *firestore.Transaction, 
+	docRef *firestore.DocumentRef, 
+	tableQrId string,
+) (bool) ***REMOVED***
+	doc, err := trx.Get(docRef)
+	if(err != nil) ***REMOVED***
+		fmt.Println("error get queueId: " + tableQrId, err.Error())
+	***REMOVED***
+	
+	// Note: try to validate using firebase rules
+	existingQueueQr := doc.Data()
+	if (existingQueueQr != nil) ***REMOVED***
+		return false
+		
+	***REMOVED***
+	fmt.Printf("response existQueueQr: %#v\n", existingQueueQr)
+	return true
+***REMOVED***
 
 func AddTableQrToQueue(tableQrId string) (*string, error) ***REMOVED***
 	tableQr, err := repository.GetOne(tableQrId)
@@ -15,40 +49,45 @@ func AddTableQrToQueue(tableQrId string) (*string, error) ***REMOVED***
 		fmt.Println(err.Error(), "[AddQRTableToQueue] error when get queue QR Data")
 		return nil, err
 	***REMOVED***
+		
+	addTableQrToQueueTransaction := func(ctx context.Context, trx *firestore.Transaction) error ***REMOVED***
+		colMerchant := fb.Client.Collection("merchant")
+		refQueue := colMerchant.Doc(tableQr.MerchantId).Collection("queue")
+		docRefQueue := refQueue.Doc(tableQr.Id)
+		
+		lastDocNumber, err := getLastDocNum(trx, refQueue)
+		if (err != nil) ***REMOVED***
+			return err
+		***REMOVED***
+		
+		now := time.Now().UTC()
 	
-	now := time.Now().UTC()
-	
-	var queueTable =  map[string]interface***REMOVED******REMOVED*** ***REMOVED***
-		"createdAt": now,
-		"updatedAt": now,
-		"name": tableQr.Name,
+		var queueTable =  map[string]interface***REMOVED******REMOVED*** ***REMOVED***
+			"createdAt": now,
+			"updatedAt": now,
+			"name": tableQr.Name,
+			"number": *lastDocNumber + 1,
+		***REMOVED***
+		fmt.Printf("response queueTable: %#v\n", queueTable)
+		
+		isAbleToGenerateNewQueue := validateGenerateNewQueue(trx, docRefQueue, tableQr.Id)
+		if (!isAbleToGenerateNewQueue) ***REMOVED***
+			return errors.New("The " + tableQrId + " table is on the queue")
+		***REMOVED***
+		
+		fbErr := trx.Set(docRefQueue, queueTable)
+		if(fbErr != nil) ***REMOVED***
+			return fbErr
+		***REMOVED***
+			
+		return nil
 	***REMOVED***
 	
-	fmt.Printf("response queueTable: %#v\n", queueTable)
-	// Noted: try to validate using firebase rules
-	
-	ref := fb.Client.Collection("merchant")
-	refQueue := ref.Doc(tableQr.MerchantId).Collection("queue")
-	doc, myErr := refQueue.Doc(tableQr.Id).Get(context.Background())
-	existingQueueQr := doc.Data()
+	errTrx := fb.Client.RunTransaction(context.Background(), addTableQrToQueueTransaction)
 		
-		
-	if(myErr != nil) ***REMOVED***
-		fmt.Println("error get queueId: " + tableQr.Id, myErr.Error())
-	***REMOVED***
-	
-	fmt.Printf("response existQueueQr: %#v\n", existingQueueQr)
-	
-	if (existingQueueQr != nil) ***REMOVED***
-		return nil, errors.New("The " + tableQr.Id + " table is on the queue")
-	***REMOVED***
-	
-	_, fbErr := refQueue.Doc(tableQr.Id).
-		Set(context.Background(), queueTable)
-		
-	if(fbErr != nil) ***REMOVED***
-		fmt.Println(err, "[AddQRTableToQueue] error when insert to firebase")
-		return nil, err
+	if(errTrx != nil) ***REMOVED***
+		fmt.Println(errTrx, "[AddQRTableToQueue] error when insert to firebase")
+		return nil, errTrx
 	***REMOVED***
 	return &tableQr.Id, nil
 ***REMOVED***
